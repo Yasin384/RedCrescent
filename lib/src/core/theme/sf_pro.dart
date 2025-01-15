32348ae37df13dcc9f8352586c0f145e29c2d4bc
vektemok <lunnoa15@gmail.com>
2025-01-15 23:04:57 +0600

    add SFPro theme extension

---
+++ lib/src/core/theme/sf_pro.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+/// s (Размер шрифта) W (Ширина шрифта)
+
+/// {@template sf_pro}
+/// SfPro.
+/// {@endtemplate}
+final class SfPro extends ThemeExtension<SfPro> {
+  /// {@macro sf_pro}
+  const SfPro();
+
+  // final TextStyle s24W500;
+
+  @override
+  ThemeExtension<SfPro> copyWith() {
+    // TODO: implement copyWith
+    throw UnimplementedError();
+  }
+
+  @override
+  ThemeExtension<SfPro> lerp(covariant ThemeExtension<SfPro>? other, double t) {
+    // TODO: implement lerp
+    throw UnimplementedError();
+  }
+}
