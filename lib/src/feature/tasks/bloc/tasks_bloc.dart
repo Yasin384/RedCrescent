b21ac83b590d4a37c1022309c847efac3b9021c7
zalkar <zalkar@192.168.1.3>
2025-01-31 17:40:58 +0600

    add tranform

--- lib/src/feature/tasks/bloc/tasks_bloc.dart
+++ lib/src/feature/tasks/bloc/tasks_bloc.dart
@@ -1,22 +1,35 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:red_crescent/src/core/model/login_exception.dart';
 import 'package:red_crescent/src/feature/tasks/data/task_repository.dart';
 import 'package:red_crescent/src/feature/tasks/model/task.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'tasks_event.dart';
 
 part 'tasks_state.dart';
 
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
+
 class TasksBloc extends Bloc<TasksEvent, TasksState> {
   TasksBloc({required TaskRepository tasksRepository})
       : _taskRepository = tasksRepository,
         super(TasksInitial()) {
-    on<TasksEvent>((event, emit) {
-      return switch (event) {
-        GetTasks() => _onGetTasks(event, emit)
-      };
-    });
+    on<TasksEvent>(
+      (event, emit) {
+        return switch (event) {
+          GetTasks() => _onGetTasks(event, emit),
+        };
+      },
+      transformer: throttleDroppable(throttleDuration),
+    );
   }
 
   final TaskRepository _taskRepository;
@@ -28,7 +41,6 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
 
       if (!isInitial && (state as TasksLoaded).hasReachedMax) return;
 
-
       emit(isInitial
           ? TasksLoading()
           : (state as TasksLoaded).copyWith(isLoadingMore: true));
@@ -36,12 +48,10 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
       final newTasks = await _taskRepository.loadTask(currentPage);
 
       emit(TasksLoaded(
-        tasks: isInitial
-            ? newTasks
-            : (state as TasksLoaded).tasks.merge(newTasks),
+        tasks:
+            isInitial ? newTasks : (state as TasksLoaded).tasks.merge(newTasks),
         hasReachedMax: newTasks.next == null,
       ));
-
     } on LoginException catch (error, stackTrace) {
       emit(TasksError('Ошибка авторизации: ${error.statusCode}'));
       onError(error, stackTrace);
@@ -61,72 +71,68 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
         : 1;
   }
 
-    int _getCurrentPage(bool isInitial) {
-      if (isInitial) return 1;
-      final currentState = state as TasksLoaded;
-      return _parsePageNumber(currentState.tasks.next);
-    }
+  int _getCurrentPage(bool isInitial) {
+    if (isInitial) return 1;
+    final currentState = state as TasksLoaded;
+    return _parsePageNumber(currentState.tasks.next);
+  }
 
-  // Future<void> _onGetInitialTasks(
-  //     GetInitialTasks event,
-  //     Emitter<TasksState> emit,
-  //     ) async {
-  //   try {
-  //     emit(TasksLoading());
-  //     final tasks = await _taskRepository.loadTask(1);
-  //
-  //     emit(TasksLoaded(tasks: tasks));
-  //   } on LoginException catch (error, stackTrace) {
-  //     emit(TasksError('Ошибка авторизации: ${error.statusCode}'));
-  //     onError(error, stackTrace);
-  //   } on Object catch (error, stackTrace) {
-  //     emit(TasksError('Произошла ошибка при загрузке данных'));
-  //     onError(error, stackTrace);
-  //   }
-  // }
-  //
-  // Future<void> _onLoadMoreTasks(
-  //     LoadMoreTasks event,
-  //     Emitter<TasksState> emit,
-  //     ) async {
-  //   if (state is! TasksLoaded) return;
-  //   final currentState = state as TasksLoaded;
-  //
-  //   if (currentState.isLoadingMore || currentState.tasks.next == null) return;
-  //
-  //   emit(currentState.copyWith(isLoadingMore: true));
-  //
-  //   try {
-  //     final nextPage = _getPageNumber(currentState.tasks.next!);
-  //     final newTasks = await _taskRepository.loadTask(nextPage);
-  //
-  //     emit(TasksLoaded(
-  //       tasks: Tasks(
-  //         count: newTasks.count,
-  //         next: newTasks.next,
-  //         previous: newTasks.previous,
-  //         results: [
-  //           ...currentState.tasks.results,
-  //           ...newTasks.results,
-  //         ],
-  //       ),
-  //       isLoadingMore: false,
-  //     ));
-  //   } on LoginException catch (error, stackTrace) {
-  //     emit(TasksError('Ошибка авторизации: ${error.statusCode}'));
-  //     onError(error, stackTrace);
-  //   } on Object catch (error, stackTrace) {
-  //     emit(TasksError('Произошла ошибка при загрузке данных'));
-  //     onError(error, stackTrace);
-  //   }
-  // }
-  //
-  // int _getPageNumber(String url) {
-  //   final uri = Uri.parse(url);
-  //   return int.parse(uri.queryParameters['page'] ?? '1');
-  // }
+// Future<void> _onGetInitialTasks(
+//     GetInitialTasks event,
+//     Emitter<TasksState> emit,
+//     ) async {
+//   try {
+//     emit(TasksLoading());
+//     final tasks = await _taskRepository.loadTask(1);
+//
+//     emit(TasksLoaded(tasks: tasks));
+//   } on LoginException catch (error, stackTrace) {
+//     emit(TasksError('Ошибка авторизации: ${error.statusCode}'));
+//     onError(error, stackTrace);
+//   } on Object catch (error, stackTrace) {
+//     emit(TasksError('Произошла ошибка при загрузке данных'));
+//     onError(error, stackTrace);
+//   }
+// }
+//
+// Future<void> _onLoadMoreTasks(
+//     LoadMoreTasks event,
+//     Emitter<TasksState> emit,
+//     ) async {
+//   if (state is! TasksLoaded) return;
+//   final currentState = state as TasksLoaded;
+//
+//   if (currentState.isLoadingMore || currentState.tasks.next == null) return;
+//
+//   emit(currentState.copyWith(isLoadingMore: true));
+//
+//   try {
+//     final nextPage = _getPageNumber(currentState.tasks.next!);
+//     final newTasks = await _taskRepository.loadTask(nextPage);
+//
+//     emit(TasksLoaded(
+//       tasks: Tasks(
+//         count: newTasks.count,
+//         next: newTasks.next,
+//         previous: newTasks.previous,
+//         results: [
+//           ...currentState.tasks.results,
+//           ...newTasks.results,
+//         ],
+//       ),
+//       isLoadingMore: false,
+//     ));
+//   } on LoginException catch (error, stackTrace) {
+//     emit(TasksError('Ошибка авторизации: ${error.statusCode}'));
+//     onError(error, stackTrace);
+//   } on Object catch (error, stackTrace) {
+//     emit(TasksError('Произошла ошибка при загрузке данных'));
+//     onError(error, stackTrace);
+//   }
+// }
+//
+// int _getPageNumber(String url) {
+//   final uri = Uri.parse(url);
+//   return int.parse(uri.queryParameters['page'] ?? '1');
+// }
 }
-
-
-
-
